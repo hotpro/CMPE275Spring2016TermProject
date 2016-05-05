3884bdbb8acdaa31b22b229cc51da5f3626d4f5d
Yutao Hou <chrishou118@gmail.com>
2016-05-05 16:36:59 -0700

    add MenuItem Controller

--- src/main/java/edu/sjsu/cmpe275/controller/MenuItemController.java
+++ src/main/java/edu/sjsu/cmpe275/controller/MenuItemController.java
@@ -1,12 +1,28 @@
 package edu.sjsu.cmpe275.controller;
 
+import edu.sjsu.cmpe275.domain.MenuItem;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by yutao on 5/5/16.
  */
 @Controller
-@RequestMapping()
+@RequestMapping("/menuitems")
 public class MenuItemController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody
+    List<MenuItem> getMenuItems() {
+        List<MenuItem> list = new ArrayList<>();
+        MenuItem item = new MenuItem();
+        item.setName("ha");
+        list.add(item);
+        return list;
+    }
 }
