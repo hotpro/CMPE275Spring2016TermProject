911cd50d28508e68f62f2c9227e2008ecc0e8145
Yutao Hou <chrishou118@gmail.com>
2016-05-05 14:04:40 -0700

    first commit

---
+++ src/main/java/edu/sjsu/cmpe275/Cmpe275Spring2016TermProjectApplication.java
@@ -0,0 +1,12 @@
+package edu.sjsu.cmpe275;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cmpe275Spring2016TermProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cmpe275Spring2016TermProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/sjsu/cmpe275/Cmpe275Spring2016TermProjectApplicationTests.java
@@ -0,0 +1,18 @@
+package edu.sjsu.cmpe275;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Cmpe275Spring2016TermProjectApplication.class)
+@WebAppConfiguration
+public class Cmpe275Spring2016TermProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
