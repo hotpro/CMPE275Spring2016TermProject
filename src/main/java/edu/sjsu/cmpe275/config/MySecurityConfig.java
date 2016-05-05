679e11e2d6df3720ff8f9cc8418a47e65d52ee1d
Yutao Hou <chrishou118@gmail.com>
2016-05-05 15:11:40 -0700

    add annotation

--- src/main/java/edu/sjsu/cmpe275/config/MySecurityConfig.java
+++ src/main/java/edu/sjsu/cmpe275/config/MySecurityConfig.java
@@ -1,13 +1,17 @@
 package edu.sjsu.cmpe275.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Created by yutao on 5/5/16.
  */
+@Configuration
+@EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
