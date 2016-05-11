9819b8951b28b8f2563c7d424d962a7ff99ec431
xiaofengjohn <xiaofenglijohn@gmail.com>
2016-05-11 11:51:28 -0700

    Add authentication on /signup.

--- src/main/java/edu/sjsu/cmpe275/config/MySecurityConfig.java
+++ src/main/java/edu/sjsu/cmpe275/config/MySecurityConfig.java
@@ -45,7 +45,7 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         
-        http.authorizeRequests().antMatchers("/", "/index", "/upload_images/**", "/signupform", "/menu").permitAll()
+        http.authorizeRequests().antMatchers("/", "/index", "/upload_images/**", "/signupform", "/menu", "/signup").permitAll()
                     .antMatchers("/static_res/**").permitAll()
                     .antMatchers("/upload_images/**").permitAll()
                     .anyRequest().authenticated()
