996b3eb274f11abf7c4ba2107cf45ed343be14be
xiaofengjohn <xiaofenglijohn@gmail.com>
2016-05-10 20:27:27 -0700

    Add salt.

--- src/main/java/edu/sjsu/cmpe275/config/DaoUserDetailsService.java
+++ src/main/java/edu/sjsu/cmpe275/config/DaoUserDetailsService.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import edu.sjsu.cmpe275.Constant;
 import edu.sjsu.cmpe275.dao.UserDao;
 import edu.sjsu.cmpe275.domain.User;
 
@@ -19,11 +20,12 @@ import edu.sjsu.cmpe275.domain.User;
 public class DaoUserDetailsService implements UserDetailsService{
 
 	@Autowired
-	private UserDao userDao ;
+    private UserDao userDao;
 	
 	public UserDao getUserDao() {
 		return userDao;
 	}
+
 
 	public void setUserDao(UserDao userDao) {
 		this.userDao = userDao;
@@ -32,6 +34,7 @@ public class DaoUserDetailsService implements UserDetailsService{
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		System.out.println(userDao);
 		List<User> user = userDao.findByEmail(username);
 		if (user.isEmpty()) {
 			throw new UsernameNotFoundException(username + " not found.");
@@ -41,14 +44,29 @@ public class DaoUserDetailsService implements UserDetailsService{
 		User u = user.get(0);
 		Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
 		authorities.add(new SimpleGrantedAuthority(u.getRole()));
-		return new org.springframework.security.core.userdetails.User(
-				   u.getEmail(),
-				   u.getPassword(),
-				   u.isActive(),
-				   true,
-				   true,
-				   true,
-				   authorities);
+		return new UserWithSalt(u.getEmail(), Constant.SALT, u.getPassword(), authorities);
+//		return new org.springframework.security.core.userdetails.User(
+//				   u.getEmail(),
+//				   u.getPassword(),
+//				   u.isActive(),
+//				   true,
+//				   true,
+//				   true,
+//				   authorities);
+	}
+	
+	
+	public class UserWithSalt extends org.springframework.security.core.userdetails.User {
+	    private String salt;
+
+	    public UserWithSalt(String username, String salt, String password, Collection<? extends GrantedAuthority> authorities) {
+	        super(username, password, authorities);
+	        this.salt = salt;
+	    }
+
+	    public String getSalt() {
+	        return salt;
+	    }
 	}
 
 }
--- src/main/java/edu/sjsu/cmpe275/config/DispatchAuthenticationSuccessHandler.java
+++ src/main/java/edu/sjsu/cmpe275/config/DispatchAuthenticationSuccessHandler.java
@@ -6,10 +6,13 @@ import java.util.Collection;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import edu.sjsu.cmpe275.domain.User;
 
 public class DispatchAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
@@ -17,11 +20,20 @@ public class DispatchAuthenticationSuccessHandler implements AuthenticationSucce
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
 			throws IOException, ServletException {
 		Collection<GrantedAuthority> authCollection = (Collection<GrantedAuthority>)authentication.getAuthorities();
+		HttpSession session = request.getSession();
+		if (session == null) {
+			return;
+		}
+		User u = new User();
+		u.setEmail(authentication.getName());
+		session.setAttribute("USER", u);
 		for (GrantedAuthority ga : authCollection) {
 			if (ga.getAuthority().equals("USER")) {
+				u.setRole(ga.getAuthority());
 				response.sendRedirect("/");
 				return;
 			} else if(ga.getAuthority().equals("ADMIN")) {
+				u.setRole(ga.getAuthority());
 				response.sendRedirect("/admin");
 				return;
 			}
